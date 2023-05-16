b7971ef894044d533f6e19ab233cc1f503a7f313
guibeal <guibeal96@gmail.com>
2023-05-16 02:33:09 -0300

    prediction

--- Python/functions.py
+++ Python/functions.py
@@ -1,5 +1,5 @@
-import pysid
 import pandas as pd
+import pysid
 import control
 
 def transferFunction(num, den):
@@ -10,10 +10,20 @@ def transferFunction(num, den):
     num_z += n * z**(-i)
   for i, d in enumerate(den):
     den_z += d * z**(-i)
-  return control.minreal(num_z/den_z)
+  return control.minreal(num_z/den_z, verbose=False)
+
+def predict(u, y, G, H, nk):
+  Lu = control.minreal(G/H,     verbose=False)
+  Ly = control.minreal(1 - 1/H, verbose=False)
+
+  y_u = control.forced_response(sys=Lu, U=u, return_x=False)
+  y_y = control.forced_response(sys=Ly, U=y, return_x=False)
+  assert(len(y_u[1]) == len(y_y[1]))
+
+  return y_u[1][nk:] + y_y[1][nk:]
 
 def models_frame():
-  return pd.DataFrame(columns=['model', 'na', 'nb', 'nc', 'nd', 'nf', 'nk', 'A', 'B', 'C', 'D', 'F', 'G', 'H'])
+  return pd.DataFrame(columns=['model', 'na', 'nb', 'nc', 'nd', 'nf', 'nk', 'A', 'B', 'C', 'D', 'F', 'G', 'H', 'yp'])
 
 def arx(u_i, y_i, u_v, y_v, na_range, nb_range, nk_range):
   models = pd.DataFrame()
@@ -30,6 +40,8 @@ def arx(u_i, y_i, u_v, y_v, na_range, nb_range, nk_range):
         G = transferFunction(B, A)
         H = transferFunction([1], A)
 
+        y_p = predict(u_v, y_v, G, H, nk)
+
         models = pd.concat([models, pd.DataFrame({
           'model': 'arx',
           'na': [na],
@@ -39,6 +51,7 @@ def arx(u_i, y_i, u_v, y_v, na_range, nb_range, nk_range):
           'B': [B],
           'G': [G],
           'H': [H],
+          'yp': [y_p],
         })])
 
   return models
@@ -60,6 +73,8 @@ def armax(u_i, y_i, u_v, y_v, na_range, nb_range, nc_range, nk_range):
           G = transferFunction(B, A)
           H = transferFunction(C, A)
 
+          y_p = predict(u_v, y_v, G, H, nk)
+
           models = pd.concat([models, pd.DataFrame({
             'model': 'armax',
             'na': [na],
@@ -71,6 +86,7 @@ def armax(u_i, y_i, u_v, y_v, na_range, nb_range, nc_range, nk_range):
             'C': [C],
             'G': [G],
             'H': [H],
+            'yp': [y_p],
           })])
 
   return models
@@ -90,6 +106,8 @@ def oe(u_i, y_i, u_v, y_v, nb_range, nf_range, nk_range):
         G = transferFunction(B, F)
         H = transferFunction([1], [1])
 
+        y_p = predict(u_v, y_v, G, H, nk)
+
         models = pd.concat([models, pd.DataFrame({
           'model': 'oe',
           'nb': [nb],
@@ -99,6 +117,7 @@ def oe(u_i, y_i, u_v, y_v, nb_range, nf_range, nk_range):
           'F': [F],
           'G': [G],
           'H': [H],
+          'yp': [y_p],
         })])
 
   return models
@@ -123,6 +142,8 @@ def bj(u_i, y_i, u_v, y_v, nb_range, nc_range, nd_range, nf_range, nk_range):
               G = transferFunction(B, F)
               H = transferFunction(C, D)
 
+              y_p = predict(u_v, y_v, G, H, nk)
+
               models = pd.concat([models, pd.DataFrame({
                 'model': 'bj',
                 'nb': [nb],
@@ -136,6 +157,7 @@ def bj(u_i, y_i, u_v, y_v, nb_range, nc_range, nd_range, nf_range, nk_range):
                 'F': [F],
                 'G': [G],
                 'H': [H],
+                'yp': [y_p],
               })])
             except Exception as e:
               # display(str(e))
